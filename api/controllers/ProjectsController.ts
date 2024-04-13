78c827eb2df60f30027cae9e03cd0bcc15ab30af
E224340Y <thomas.ambroise@etu.univ-nantes.fr>
2024-04-13 14:06:54 +0200

    Remove old server.ts and api folder

--- api/controllers/ProjectsController.ts
+++
@@ -1,8 +0,0 @@
-import Project from "../models/Project"
-
-// Projects Controller
-export default {
-    findAll: (): Promise<Project[]> => { throw new Error('Not implemented yet') },
-    findById: (): Promise<Project> => { throw new Error('Not implemented yet') },
-    add: (project: Project): Promise<boolean> => { throw new Error('Not implemented yet') }
-}
