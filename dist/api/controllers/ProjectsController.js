e4ac9f309393245db066cfb3edeb1b0851403c2f
E224340Y <thomas.ambroise@etu.univ-nantes.fr>
2024-04-13 14:06:15 +0200

    Remove dist

--- dist/api/controllers/ProjectsController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Projects Controller
-exports.default = {
-    findAll: () => { throw new Error('Not implemented yet'); },
-    findById: () => { throw new Error('Not implemented yet'); },
-    add: (project) => { throw new Error('Not implemented yet'); }
-};
--- dist/api/routes/router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router
-    .get('/', (_, res) => { res.send({ message: 'Server is running' }); });
-exports.default = router;
